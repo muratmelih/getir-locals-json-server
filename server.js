70fdbcdc0ecd80b7b5536e7fe97a851f870c759f
Melih Işık <melih.isik@solutions.consulting>
2021-12-06 21:29:54 +0300

    cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const jsonServer = require("json-server");
+const cors = require("cors");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
@@ -6,6 +7,7 @@ const port = process.env.PORT || 3003;
 
 server.use(middlewares);
 server.use(router);
+server.use(cors);
 server.listen(port, () => {
   console.log(`Server listening on ${port}`);
 });
