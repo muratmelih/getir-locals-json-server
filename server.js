f2fb3710a6c69d59beef23ec34e08c0c9ba55bed
Melih Işık <melih.isik@solutions.consulting>
2021-12-08 11:25:12 +0300

    fix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3003;
-//
+
 server.use(middlewares);
 server.use(router);
 server.use(cors);
